a018475f72c7f963f23e2825ac1e9e8abdcc21d2
kounelios13 <pkounelios@gmail.com>
2017-03-21 22:04:55 +0200

    Formmating.Changed identation from tabs to spaces

--- dnd.js
+++ dnd.js
@@ -2,59 +2,59 @@
 * @param event A jQuery event that occurs when an .droppable is being dragged
 */
 function dragStartHandler(event){
-	//e refers to a jQuery object
-	//that does not have dataTransfer property
-	//so we have to refer to the original javascript event
-	var originalEvent = event.originalEvent;
-	//We want to store the data-task-id of the object that is being dragged
-	originalEvent.dataTransfer.setData("text",$(event.target).data("task-id"));
-	originalEvent.dataTransfer.effectAllowed = "move";
+    //e refers to a jQuery object
+    //that does not have dataTransfer property
+    //so we have to refer to the original javascript event
+    var originalEvent = event.originalEvent;
+    //We want to store the data-task-id of the object that is being dragged
+    originalEvent.dataTransfer.setData("text",$(event.target).data("task-id"));
+    originalEvent.dataTransfer.effectAllowed = "move";
 }
 /*
 * @param event A jQuery event that occurs when a .droppable as been dropped
 */
 function dropHandler(event){
-	event.preventDefault();
-	event.stopPropagation();
-	var originalEvent = event.originalEvent;
-	//Get the task-id of the dropped item
-	var droppedItemId = originalEvent.dataTransfer.getData("text");
-	//Find the dropped item by using its data-task-id attribute
-	var droppedItem = $("body").find(`[data-task-id='${droppedItemId}']`);
-	//Find the category which it was dragged into
-	var category = $(this).parent(".box").data("category");
-	//Change the data-category-group of the dropped item
-	//and move it from its original position to the new one
-	droppedItem.data("category-group",category).appendTo($(this));
+    event.preventDefault();
+    event.stopPropagation();
+    var originalEvent = event.originalEvent;
+    //Get the task-id of the dropped item
+    var droppedItemId = originalEvent.dataTransfer.getData("text");
+    //Find the dropped item by using its data-task-id attribute
+    var droppedItem = $("body").find(`[data-task-id='${droppedItemId}']`);
+    //Find the category which it was dragged into
+    var category = $(this).parent(".box").data("category");
+    //Change the data-category-group of the dropped item
+    //and move it from its original position to the new one
+    droppedItem.data("category-group",category).appendTo($(this));
 }
 $(document).ready(function(){
-	//When a new task/item is creatted it is assigned a unique data attribute which is the task index
-	var taskIndex = 0;
-	$(".createTask").on("click",function(){
-		//Find the category whict this button belongs to 
-		var currentCategory = $(this).parent("h1").parent(".box");
-		var categoryId = currentCategory.data("category");
-		//Create a new task
-		var task = $("<div class='list-group-item droppable' draggable='true' data-task-id="+taskIndex+"></div>");
-		//Ask the user for a task description
-		var taskDescription = prompt("Enter description for yout task");
-		if(taskDescription){
-			//If the user wants to create a non empty task
-			task.text(taskDescription);
-			taskIndex++;
-		}else{
-			//User has left the task description empty
-			//so exit
-			return;
-		}
-		//Append the new task to the clossest dropTarget
-		//by finding the dropTarget that is available in our currentCategory
-		task.appendTo(currentCategory.find(".dropTarget"));
-	});
-	$("body").on("dragstart",".droppable",dragStartHandler);
-	$(".dropTarget").on("dragenter",function(event){
-		event.preventDefault();
-		event.stopPropagation();
-	}).on("dragover",false)
-	.on("drop",dropHandler);
+    //When a new task/item is creatted it is assigned a unique data attribute which is the task index
+    var taskIndex = 0;
+    $(".createTask").on("click",function(){
+        //Find the category whict this button belongs to 
+        var currentCategory = $(this).parent("h1").parent(".box");
+        var categoryId = currentCategory.data("category");
+        //Create a new task
+        var task = $("<div class='list-group-item droppable' draggable='true' data-task-id="+taskIndex+"></div>");
+        //Ask the user for a task description
+        var taskDescription = prompt("Enter description for yout task");
+        if(taskDescription){
+            //If the user wants to create a non empty task
+            task.text(taskDescription);
+            taskIndex++;
+        }else{
+            //User has left the task description empty
+            //so exit
+            return;
+        }
+        //Append the new task to the clossest dropTarget
+        //by finding the dropTarget that is available in our currentCategory
+        task.appendTo(currentCategory.find(".dropTarget"));
+    });
+    $("body").on("dragstart",".droppable",dragStartHandler);
+    $(".dropTarget").on("dragenter",function(event){
+        event.preventDefault();
+        event.stopPropagation();
+    }).on("dragover",false)
+    .on("drop",dropHandler);
 });
